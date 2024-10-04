7aa14edc318b991a04912b01bbc1e8359c4e21b0
Efrain Colque Calizaya <efraincolquecalizaya@gmail.com>
2024-10-04 14:50:07 -0400

    add: frontend files

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,17 +5,16 @@ import DashboardAdmin from "../components/DashboardAdmin";
 import Register from "../components/register/Register";
 import Home from "../pages/admin/Home";
 import Login from "../components/Login";
+
 function AppRoutes() {
   return (  
     <RouterRoutes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      {/* CAMPOS DE REGISTRO DE CAJERO/}
-      {/* CAMPOS DE REGISTRO DE USUARIOS*/}
-      {/* CAMPOS DE REGISTRO DE ADMIN*/}
       <Route path="/usuario/*" element={<DashboardUsuario />} />
       <Route path="/cajero/*" element={<DashboardCajero />} />
       <Route path="/admin/*" element={<DashboardAdmin />} />
+      {/* Aquí puedes definir el componente Home dentro de DashboardAdmin */}
       <Route path="/admin/home" element={<Home />} />
       <Route path="*" element={<Navigate to="/" />} /> {/* Redirige cualquier ruta desconocida */}
     </RouterRoutes>
@@ -23,4 +22,3 @@ function AppRoutes() {
 }
 
 export default AppRoutes;
-n
